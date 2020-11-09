2ef314e7e7f228d116734528b15b2974b1cb1f6d
Konstantin Winkelmayer <S2010438039@fhooe.at>
2020-11-09 15:23:27 +0100

    classes and objects added

---
+++ Classes/Classes/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    public class Person
+    {
+       public string Name;
+
+
+        public void Print()
+        {
+            Console.WriteLine("I am " + Name);
+        }
+    }
+}
---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Classes!");
+
+            //class ----- instantiate -------->  objects
+
+
+            //instantiate object
+            //string name = "Max Mustermann"
+            string name = new string("Max Mustermann");
+
+
+            //create object
+            Person person = new Person();
+            person.Name = name;
+            
+
+            //create object
+            Person person2 = new Person();
+            person2.Name = "Peter";
+
+            //use objects
+            person.Print();
+            person2.Print();
+           
+        }
+    }
+}
